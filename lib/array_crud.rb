2bba72752b54eea7dd124b19047b8046fd611a15
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-01 22:00:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,11 +16,13 @@ end
 
 def add_element_to_start_of_array(array, element)
 
-  array.unshift(element) 
+  array.unshift(element)
 
 end
 
 def remove_element_from_end_of_array(array)
+
+  array.pop
 
 end
 
